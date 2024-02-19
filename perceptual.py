1c8a0c948941700966e32fd0d0516d3df4034df1
yeftawatson <yeftanathaniel16@gmail.com>
2024-02-19 13:27:45 +0700

    menu jadi, tapi blm work

--- perceptual.py
+++ perceptual.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from scipy.fftpack import dct
+from tkinter import Tk, filedialog
+
 
 def dct_hash(image, hash_size=8):
     # Konversi gambar ke grayscale
@@ -39,18 +41,56 @@ def authenticate_image(image1, image2, threshold):
         return True
     else:
         return False
+    
+# Fungsi untuk memilih gambar
+def pilih_gambar():
+    root = Tk()
+    root.withdraw()  # Sembunyikan jendela root
+    file_path = filedialog.askopenfilename()  # Buka jendela dialog pemilihan file
+    return file_path
 
 # Contoh penggunaan
 if __name__ == "__main__":
     # Baca gambar
-    image1 = cv2.imread("gambar1.png")
-    image2 = cv2.imread("gambar2.png")
+    print("Silahkan Pilih Gambar yang terautentikasi")
+    gambar1 = pilih_gambar()
+    print("Silahkan Pilih Gambar yang akan di cek autentikasinya")
+    gambar2 = pilih_gambar()
+
+    image1 = cv2.imread(gambar1)
+    image2 = cv2.imread(gambar2)
+    manipulated = image2.copy()
     
+    nama = 1
+    while nama != 0:
+        print("MENU MANIPULASI \n 1. Up Scaling \n 2. Down Scaling \n 3. Merubah Intensitas \n 4. Compress Size \n 0. Exit")
+        nama = int(input("Masukkan Pilihan: "))
+        if nama == 1:
+            faktor_skala = float(input("Masukkan Skala (Angka >1): "))
+            manipulated = cv2.resize(image2, None, fx=faktor_skala, fy=faktor_skala, interpolation=cv2.INTER_AREA)
+            break
+        elif nama == 2:
+            faktor_skala = float(input("Masukkan Skala (Angka <1): "))
+            manipulated = cv2.resize(image2, None, fx=faktor_skala, fy=faktor_skala, interpolation=cv2.INTER_AREA)
+            break
+        elif nama == 3:
+            gamma = int(input("Masukkan Skala (Contoh: 50): "))
+            manipulated = np.clip(image2 + gamma, 0, 255)
+            break
+        elif nama == 4:
+            compress = int(input("Masukkan Skala 0-100 (Higher scale, better image): "))
+            cv2.imwrite('gambar_kompres.jpg', image2, [cv2.IMWRITE_JPEG_QUALITY, compress])
+            break
+        elif nama == 0:
+            break
+        else:
+            print("Input Salah")
+
     # Set ambang batas
     threshold = 10
     
     # Autentikasi gambar
-    is_authenticated = authenticate_image(image1, image2, threshold)
+    is_authenticated = authenticate_image(image1, manipulated, threshold)
     
     # Output hasil autentikasi
     if is_authenticated:
